052f68fda065e3181eb8197f4f2ee4cb6ac8cad1
dvskullz <sharmadaksh275@gmail.com>
2025-08-02 20:03:04 +0530

    Implement Transaction with inputs/outputs, ID, and digital signature

--- src/Transaction.java
+++ src/Transaction.java
@@ -13,6 +13,9 @@ public class Transaction {
     public ArrayList<TransactionInputs> inputs = new ArrayList<TransactionInputs>();
     public ArrayList<TransactionOutputs> outputs = new ArrayList<TransactionOutputs>();
 
+//    public ArrayList<TransactionInputs> inputs = new ArrayList<TransactionInputs>();
+//    public ArrayList<TransactionOutputs> outputs = new ArrayList<TransactionOutputs>();
+
     private static int sequence = 0; // a rough count of how many transactions have been generated.
 
     // Constructor:
@@ -20,7 +23,12 @@ public class Transaction {
         this.sender = from;
         this.recipient = to;
         this.value = value;
-        this.inputs = inputs;
+
+        if (inputs == null) {
+            this.inputs = new ArrayList<>();
+        } else {
+            this.inputs = inputs;
+        }
     }
 
     // This Calculates the transaction hash (which will be used as its Id)
@@ -45,4 +53,63 @@ public class Transaction {
         return StringUtil.verifyECDSASig(sender, data, signature);
 
     }
+    //Returns true if new transaction could be created.
+    public boolean processTransaction() {
+
+        if(verifySignature() == false) {
+            System.out.println("#Transaction Signature failed to verify");
+            return false;
+        }
+
+        //gather transaction inputs (Make sure they are unspent):
+        for(TransactionInputs i : inputs) {
+            i.UTXO = CoreChain.UTXOs.get(i.transactionOutputId);
+        }
+
+        //check if transaction is valid:
+        if(getInputsValue() < CoreChain.minimumTransaction) {
+            System.out.println("#Transaction Inputs to small: " + getInputsValue());
+            return false;
+        }
+
+        //generate transaction outputs:
+        float leftOver = getInputsValue() - value; //get value of inputs then the left over change:
+        transactionId =  calculateHash();
+        outputs.add(new TransactionOutputs( this.recipient, value,transactionId)); //send value to recipient
+        outputs.add(new TransactionOutputs( this.sender, leftOver,transactionId)); //send the left over 'change' back to sender
+
+        //add outputs to Unspent list
+        for(TransactionOutputs o : outputs) {
+            CoreChain.UTXOs.put(o.id , o);
+        }
+
+        //remove transaction inputs from UTXO lists as spent:
+        for(TransactionInputs i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it
+            CoreChain.UTXOs.remove(i.UTXO.id);
+        }
+
+        return true;
+    }
+
+    //returns sum of inputs(UTXOs) values
+    public float getInputsValue() {
+        float total = 0;
+//        if (inputs == null) return 0; //
+        for(TransactionInputs i : inputs) {
+            if(i.UTXO == null) continue; //if Transaction can't be found skip it
+            total += i.UTXO.value;
+        }
+        return total;
+    }
+
+    //returns sum of outputs:
+    public float getOutputsValue() {
+        float total = 0;
+        for(TransactionOutputs o : outputs) {
+            total += o.value;
+        }
+        return total;
+    }
+
 }
