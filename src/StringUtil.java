58539697d324cf4cb5f334ca3657223a4fc5e70f
dvskullz <sharmadaksh275@gmail.com>
2025-08-02 20:03:19 +0530

    Add cryptographic utility functions: SHA-256, ECDSA, Merkle root

--- src/StringUtil.java
+++ src/StringUtil.java
@@ -11,10 +11,15 @@
  * Author: [dvskullz]
  */
 
+
 import java.security.*;
+import java.util.ArrayList;
 import java.util.Base64;
+import com.google.gson.GsonBuilder;
+import java.util.List;
 
 public class StringUtil {
+
     public static String applySha256(String input){
         try {
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
@@ -60,8 +65,35 @@ public class StringUtil {
             throw new RuntimeException(e);
         }
     }
+    //Short hand helper to turn Object into a json string
+    public static String getJson(Object o) {
+        return new GsonBuilder().setPrettyPrinting().create().toJson(o);
+    }
+    //Returns difficulty string target, to compare to hash. eg difficulty of 5 will return "00000"
+    public static String getDificultyString(int difficulty) {
+        return new String(new char[difficulty]).replace('\0', '0');
+    }
 
     public static String getStringFromKey(Key key) {
         return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+    //Tacks in array of transactions and returns a merkle root.
+    public static String getMerkleRoot(ArrayList<Transaction> transactions) {
+        int count = transactions.size();
+        ArrayList<String> previousTreeLayer = new ArrayList<String>();
+        for(Transaction transaction : transactions) {
+            previousTreeLayer.add(transaction.transactionId);
+        }
+        ArrayList<String> treeLayer = previousTreeLayer;
+        while(count > 1) {
+            treeLayer = new ArrayList<String>();
+            for(int i=1; i < previousTreeLayer.size(); i++) {
+                treeLayer.add(applySha256(previousTreeLayer.get(i-1) + previousTreeLayer.get(i)));
+            }
+            count = treeLayer.size();
+            previousTreeLayer = treeLayer;
+        }
+        String merkleRoot = (treeLayer.size() == 1) ? treeLayer.get(0) : "";
+        return merkleRoot;
     }
 }
