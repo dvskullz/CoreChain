506c089058542addb23e5eac13bb98fc41a7e66b
Daksh <sharmadaksh275@gmail.com>
2025-08-02 20:06:50 +0530

    references to UTXOs

--- src/TransactionInputs.java
+++ src/TransactionInputs.java
@@ -1,3 +1,4 @@
+
 public class TransactionInputs {
         public String transactionOutputId; //Reference to TransactionOutputs -> transactionId
         public TransactionOutputs UTXO; //Contains the Unspent transaction output
