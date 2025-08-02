4e240767ad73e7765c5f269701b843b916dbb682
dvskullz <sharmadaksh275@gmail.com>
2025-08-02 20:02:26 +0530

    Add Wallet class with ECC key generation, balance check, and sendFunds logic

--- src/Wallet.java
+++ src/Wallet.java
@@ -1,5 +1,7 @@
 import java.security.*;
 import java.security.spec.ECGenParameterSpec;
+import java.util.ArrayList;
+import java.util.Map;
 
 public class Wallet {
     public PrivateKey privateKey;
@@ -23,5 +25,42 @@ public class Wallet {
         }catch(Exception e) {
             throw new RuntimeException(e);
         }
+    }
+    //returns balance and stores the UTXO's owned by this wallet in this.UTXOs
+    public float getBalance() {
+        float total = 0;
+        for (Map.Entry<String, TransactionOutputs> item: CoreChain.UTXOs.entrySet()){
+            TransactionOutputs UTXO = item.getValue();
+            if(UTXO.isMine(publicKey)) { //if output belongs to me ( if coins belong to me )
+                CoreChain.UTXOs.put(UTXO.id,UTXO); //add it to our list of unspent transactions.
+                total += UTXO.value ;
+            }
+        }
+        return total;
+    }
+    //Generates and returns a new transaction from this wallet.
+    public Transaction sendFunds(PublicKey _recipient,float value ) {
+        if(getBalance() < value) { //gather balance and check funds.
+            System.out.println("#Not Enough funds to send transaction. Transaction Discarded.");
+            return null;
+        }
+        //create array list of inputs
+        ArrayList<TransactionInputs> inputs = new ArrayList<TransactionInputs>();
+
+        float total = 0;
+        for (Map.Entry<String, TransactionOutputs> item: CoreChain.UTXOs.entrySet()){
+            TransactionOutputs UTXO = item.getValue();
+            total += UTXO.value;
+            inputs.add(new TransactionInputs(UTXO.id));
+            if(total > value) break;
+        }
+
+        Transaction newTransaction = new Transaction(publicKey, _recipient , value, inputs);
+        newTransaction.generateSignature(privateKey);
+
+        for(TransactionInputs input: inputs){
+            CoreChain.UTXOs.remove(input.transactionOutputsId);
+        }
+        return newTransaction;
     }
 }
